fc30ae625b3547440616b02f744d61243687afdc
danielle bemfica <danielleludovicoo@gmail.com>
2025-05-06 17:11:09 +0200

    practica terminada

--- Practica2/src/main/java/org/example/practica2/HelloApplication.java
+++ Practica2/src/main/java/org/example/practica2/HelloApplication.java
@@ -6,15 +6,46 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
+    public static Connection conexion;
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        String host = "jdbc:mariadb://localhost:3307/";
+        String user = "root";
+        String psw = "";
+        String bd = "prueba";
+        System.out.println("Conectando...");
+
+        try {
+            conexion = DriverManager.getConnection(host + bd, user, psw);
+            System.out.println("Conexión realizada con éxito.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("CRUD");
         stage.setScene(scene);
         stage.show();
+
+    }
+
+    public void stop(){
+        try {
+            if (conexion!=null&&!conexion.isClosed()){
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
--- Practica2/src/main/java/org/example/practica2/HelloController.java
+++ Practica2/src/main/java/org/example/practica2/HelloController.java
@@ -1,5 +1,6 @@
 package org.example.practica2;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -9,17 +10,31 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.sql.*;
 import java.time.LocalDate;
-import java.util.Date;
+
+import static org.example.practica2.HelloApplication.conexion;
 
 public class HelloController {
+    public static Estudiante editarEstudiante;
+    @FXML
+    private Button guardar;
+
+    @FXML
+    private Button botonEditar;
+
+    @FXML
+    private Button botonEliminar;
+    @FXML
+    private Button botonGuardar;
+
     @FXML
     private TextField introducirNia;
     @FXML
     private TextField introducirNombre;
     @FXML
     private DatePicker fecha;
-    
+
     @FXML
     private TableColumn<Estudiante, String> columnaNombre;
     @FXML
@@ -31,35 +46,144 @@ public class HelloController {
 
     private ObservableList<Estudiante> listaEstudiantes = FXCollections.observableArrayList();
 
-    @FXML
-    public void initialize() {
-
-        columnaNombre.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getNombre()));
-        columnaNia.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getNia()).asObject());
-        columnaFechaNacimiento.setCellValueFactory(data -> new SimpleObjectProperty<>(data.getValue().getFecha_nacimiento()));
-        tablaEstudiantes.setItems(listaEstudiantes);
-
+    private Connection connection() {
+        return conexion;
     }
 
+    public void consultaBases() {
+        String query = "SELECT * FROM estudiantes";
+        Statement stmt;
+        ResultSet respuesta;
 
+        try {
+            stmt = conexion.createStatement();
+            respuesta = stmt.executeQuery(query);
+
+            while (respuesta.next()) {
+                int nia = respuesta.getInt("nia");
+                String nombre = respuesta.getString("nombre");
+                Date fecha_nacimiento = respuesta.getDate("fecha_nacimiento");
+                System.out.println("NIA: " + nia + " - Nombre: " + nombre + " - Fecha de nacimiento: " + fecha_nacimiento);
+                listaEstudiantes.add(new Estudiante(nia, nombre, fecha_nacimiento.toLocalDate()));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        botonGuardar.setDisable(true);
+        tablaEstudiantes.getItems().clear();
+        consultaBases();
+        columnaNombre.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getNombre()));
+        columnaNia.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getNia()).asObject());
+        columnaFechaNacimiento.setCellValueFactory(data ->
+                new ReadOnlyObjectWrapper<>(data.getValue().getFecha_nacimiento())
+        );
+        tablaEstudiantes.setItems(listaEstudiantes);
+    }
+
+    @FXML
     public void insertarEstudiante() {
         String nombre = introducirNombre.getText();
         int nia = 0;
         LocalDate fechaNacimiento = fecha.getValue();
-        
-        
+
         try {
-             nia = Integer.parseInt(introducirNia.getText());
+            nia = Integer.parseInt(introducirNia.getText());
         } catch (NumberFormatException e) {
             System.out.println("NIA inválido.");
         }
-        Estudiante estudiante = new Estudiante(nia, nombre, fechaNacimiento);
 
-        listaEstudiantes.add(estudiante);
+        String query = "INSERT INTO estudiantes (nia, nombre, fecha_nacimiento) VALUES (" + nia + ",'" + nombre + "','" + java.sql.Date.valueOf(fechaNacimiento) + "');";
 
-        introducirNia.clear();
+        Statement stmt;
+
+        try {
+            stmt = conexion.createStatement();
+            stmt.executeUpdate(query);
+            listaEstudiantes.add(new Estudiante(nia, nombre, fechaNacimiento));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
         introducirNombre.clear();
+        introducirNia.clear();
         fecha.setValue(null);
+        initialize();
 
+    }
+
+    public void borrarEstudiante(Estudiante estudiante) {
+        int nia = Integer.parseInt(String.valueOf(estudiante.getNia()));
+        String nombre = String.valueOf(estudiante.getNombre());
+        LocalDate fechaNacimiento = Date.valueOf(estudiante.getFecha_nacimiento()).toLocalDate();
+
+        String query = "DELETE FROM estudiantes WHERE nia = " + nia + " and nombre = '" + nombre + "' and fecha_nacimiento = '" + fechaNacimiento + "'";
+        Statement stmt;
+
+        try {
+            stmt = conexion.createStatement();
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        initialize();
+    }
+
+    @FXML
+    public void botonEliminar() {
+        Estudiante seleccionado = tablaEstudiantes.getSelectionModel().getSelectedItem();
+
+        if (seleccionado != null) {
+            borrarEstudiante(seleccionado);
+        } else {
+            System.out.println("No hay ninguna fila seleccionada.");
+        }
+    }
+
+    @FXML
+    public void botonEditar() {
+        guardar.setDisable(true);
+        botonGuardar.setDisable(false);
+
+        editarEstudiante = tablaEstudiantes.getSelectionModel().getSelectedItem();
+
+        if (editarEstudiante != null) {
+            introducirNia.setText(String.valueOf(editarEstudiante.getNia()));
+            introducirNombre.setText(editarEstudiante.getNombre());
+            fecha.setValue(editarEstudiante.getFecha_nacimiento());
+
+        } else {
+            System.out.println("No hay ninguna fila seleccionada.");
+        }
+    }
+
+    public void guardarEstudiante() {
+        int nia = Integer.parseInt(introducirNia.getText());
+        String nombre = introducirNombre.getText();
+        LocalDate fechaNacimiento = fecha.getValue();
+
+        String query = "UPDATE estudiantes SET nombre = '" + nombre + "', nia=" + nia + ",fecha_nacimiento='" + fechaNacimiento + "' " +
+                "WHERE nombre = '" + editarEstudiante.getNombre() + "' and nia=" + editarEstudiante.getNia() + " and fecha_nacimiento = '" + editarEstudiante.getFecha_nacimiento() + "'";
+
+        Statement stmt;
+        try {
+            stmt = conexion.createStatement();
+            stmt.executeUpdate(query);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        guardar.setDisable(false);
+        botonGuardar.setDisable(true);
+
+        initialize();
     }
 }
